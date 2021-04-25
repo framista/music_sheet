87479b92ccf76b1bd71849bb9f80bebb6a0c94b9
framista <adajab1@gmail.com>
2021-04-25 15:25:57 +0200

    Backend to get and post file

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const fs = require('fs');
+const app = express();
+
+app.use(express.static('public'));
+app.use(cors());
+app.use(fileUpload());
+
+app.get('/musicsheet', (req, res) => {
+  const id = req.query.file;
+  fs.readFile(`${__dirname}/public/${id}.pdf`, function (err, data) {
+    res.contentType('application/pdf');
+    res.send(data);
+  });
+});
+
+app.post('/musicsheet', (req, res) => {
+  const { id } = req.query;
+  if (!req.files) {
+    return res.status(500).send({ msg: 'File is not found' });
+  }
+  const myFile = req.files.file;
+  myFile.mv(`${__dirname}/public/${id}.pdf`, function (err) {
+    if (err) {
+      return res.status(500).send({ msg: 'Error occured' });
+    }
+    return res.send({ name: `${id}.pdf`, path: `/${id}.pdf` });
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
