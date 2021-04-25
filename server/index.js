fe0d3f01701a5aba7a5803e4cb8e827921918d46
framista <adajab1@gmail.com>
2021-04-25 17:45:05 +0200

    Change server to connect with client

--- server/index.js
+++ server/index.js
@@ -30,6 +30,11 @@ app.post('/musicsheet', (req, res) => {
   });
 });
 
+app.use(express.static(path.join(__dirname, './client/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve('./client', 'build', 'index.html'))
+);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
